68c3afdad652b3c1c5e844b193edb565540d4cfe
Deema-Thnabi <deemathnabi25@gmail.com>
2024-09-29 14:49:29 +0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EF_create_product.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_create_product.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Task_Product; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.Name).HasColumnType("varchar(30)");
+            modelBuilder.Entity<Product>().Property(p => p.Description).HasColumnType("varchar(60)");
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(12,2)");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using EF_create_product.Data;
+using EF_create_product.Models;
+
+namespace EF_create_product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+
+            // var products = new List<Product>(){}
+            dbContext.AddRange(new List<Product>()
+         {
+        new Product()
+        {
+            Name = "Laptop",
+            Description = "High performance laptop.",
+            Price = 999.99m
+        },
+        new Product()
+        {
+            Name = "Smartphone",
+            Description = "Latest model smartphone.",
+            Price = 799.99m
+        },
+        new Product()
+        {
+            Name = "Tablet",
+            Description = "Portable tablet with long battery life.",
+            Price = 499.99m
+        },
+        new Product()
+        {
+            Name = "Headphones",
+            Description = "Noise-cancelling over-ear headphones.",
+            Price = 199.99m
+        },
+        new Product()
+        {
+            Name = "Smartwatch",
+            Description = "Fitness tracking smartwatch.",
+            Price = 149.99m
+        }
+    });
+
+            dbContext.SaveChanges();
+        }
+    }
+}
